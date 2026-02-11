f9c91127277641028c8276068caa3b2c2b01ff12
Koruto <flyingshadow03@gmail.com>
2026-02-12 02:29:23 +0530

    fix: add speed insights

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Layout } from './components/Layout'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
@@ -22,6 +23,7 @@ export default function App() {
         </Route>
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   )
 }
